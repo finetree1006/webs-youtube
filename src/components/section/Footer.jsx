22b9d6bb268916c152e1531b1d27ad757a03f2ae
finetree1006 <finetree1006@naver.com>
2024-10-13 14:37:38 +0900

    헤더 영역 완성하기

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-      <footer id='footer' role='contentinfo'>
-        Footer
-      </footer>
-  )
+    return (
+        <footer id='footer' role="contentinfo"> 
+            footer
+        </footer>
+    )
 }
 
 export default Footer
--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Main = () => {
-  return (
-        <main id='main' role='main'>
-            Main
+    return (
+        <main id="main" role="main">
+            main  
         </main>
-  )
+    )
 }
 
 export default Main
