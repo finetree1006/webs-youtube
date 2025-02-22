dcf7c1f9bc1d5171c197e7c99b32dcb033334c8d
finetree1006 <finetree1006@naver.com>
2025-02-22 18:07:47 +0900

    이름 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,8 @@ import Youtube from '../components/contents/Youtube'
 const Home = () => {
     return (
         <Main 
-            title = "웹스토리보이 유튜브"
-            description="웹스토리보이 유튜버 사이트에 오신 것을 환영합니다."
+            title = "상현 유튜브"
+            description="상현의 사이트에 오신 것을 환영합니다."
         >
             <Today />
             <Developer />
